947713a0196bc95b49ac1922282381b4f5b7525f
Mirabror Sodikov <mirabror9545@gmail.com>
2021-08-13 17:45:20 +0500

    redux middlewares

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,15 @@
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+import async from "middlewares/async";
 import reducers from "reducers";
+import stateValidator from "middlewares/stateValidator";
 
 // eslint-disable-next-line
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
 
   return <Provider store={store}>{children}</Provider>;
---
+++ src/middlewares/async.js
@@ -0,0 +1,11 @@
+export default ({ dispatch }) =>
+  (next) =>
+  (action) => {
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    action.payload.then((res) => {
+      const newAction = { ...action, payload: res };
+      dispatch(newAction);
+    });
+  };
